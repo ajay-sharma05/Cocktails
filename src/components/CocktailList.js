840a14e3a51d555102be22f1ccecf23bb077c6f0
AjayAlphonic05 <122855412+AjayAlphonic05@users.noreply.github.com>
2024-02-01 23:24:42 +0530

    first commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
+
+const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No cocktails match your search.</h2>;
+  }
+  return (
+    <section className="section">
+      <h1 className="section-title">Cocktails</h1>
+
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla at sint
+        qui nemo ex. Similique commodi assumenda inventore distinctio, odit
+        fugit quibusdam dicta numquam qui cum culpa dignissimos esse laborum
+        laudantium dolor doloribus voluptas quasi reprehenderit est adipisci
+        impedit eum ex quae maxime. Eius dolorum delectus recusandae vero veniam
+        laborum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
